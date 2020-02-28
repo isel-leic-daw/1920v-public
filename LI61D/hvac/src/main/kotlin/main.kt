0a2b26eae7fb03dcb5c0a17bf214873bcbef288c
Paulo Pereira <palbp.giga@gmail.com>
2020-02-28 19:21:52 +0000

    Adds HVAC's skeleton project

---
+++ LI61D/hvac/src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+/**
+ * The application's entry point.
+ *
+ * Execution is parametrized through the command line, as follows:
+ * TODO: Document accepted commands
+ */
+fun main() {
+    println("When I grow up, I'll be an HVAC simulator")
+}
