fe05cb3c971c0b3444d3c513a766701f7d07de82
Paulo Pereira <palbp.giga@gmail.com>
2020-04-30 18:19:06 +0100

    Removes near_future code that became present day code =)

--- LI61D/hvac-ui/app/src/App.js
+++ LI61D/hvac-ui/app/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import './App.css'
-import TemperatureCard from './temperature/Card'
-import TemperaturePage from './near_future/TemperaturePage'
 import HvacControl from './near_future/HvacControl'
-import { getTemperatureService as TemperatureService, getMockedTemperatureService as MockedTemperatureService } from './temperature/Service'
+import { 
+  getTemperatureService as TemperatureService, 
+  getMockedTemperatureService as MockedTemperatureService 
+} from './temperature/Service'
 import Page from './temperature/Page'
 
 const temperatureService = TemperatureService()
 //const temperatureService = MockedTemperatureService()
 
-
 function App() {
-  const temperature = {
-    current: { value: 26, label: "Current" },
-    target: { value: 23, label: "Target" }
-  }
   const nearFutureControl = undefined // <HvacControl />
-  const nearFutureTemperature = undefined // <TemperaturePage temperatureService={temperatureService} />
   return (
     <div className="App">
       {nearFutureControl}
-      {nearFutureTemperature}
       <Page temperatureService = {temperatureService} />
     </div>
   )
--- LI61D/hvac-ui/app/src/temperature/Display.js
+++ LI61D/hvac-ui/app/src/temperature/Display.js
@@ -4,10 +4,9 @@ import React from 'react'
  * Used to display a temperature value, along with its label
  */
 function Display({ value, label }) {
-  const displayValue = value || "--"
   return (
     <div className="ui huge statistic">
-      <div className="value"> {displayValue}&deg;</div>
+      <div className="value"> {value || "--"}&deg;</div>
       <div className="label"> {label} </div>
     </div>  
   )
