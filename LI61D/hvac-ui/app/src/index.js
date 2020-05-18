95f02eb3edfb9349e2e3b39985c71f10d1371352
Paulo Pereira <palbp.giga@gmail.com>
2020-05-18 12:44:13 +0100

    LI61D: Fixes minor formatting issue in HVAC UI code

--- LI61D/hvac-ui/app/src/index.js
+++ LI61D/hvac-ui/app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './main/App'
 import * as serviceWorker from './serviceWorker'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
